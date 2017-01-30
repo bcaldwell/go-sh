67160723b5733b6fbc9ade3a75126bc558867666
Benjamin Caldwell <benjamincaldwell@users.noreply.github.com>
2017-01-30 17:48:22 -0500

    Fix output not returning errors

--- mock/sh_mock.go
+++ mock/sh_mock.go
@@ -1,9 +1,9 @@
 package shMock
 
 import (
-    "strings"
+	"strings"
 
-    "github.com/benjamincaldwell/go-sh"
+	"github.com/benjamincaldwell/go-sh"
 )
 
 // list of commands that go run
@@ -15,73 +15,87 @@ var ErrorsToReturn []error
 // top element is popped and returned from output function
 var OutputToRetrun []string
 
-func Reset() {
-    Commands = Commands[:0]
-    ErrorsToReturn = ErrorsToReturn[:0]
-    OutputToRetrun = OutputToRetrun[:0]
-}
+var DefaultShInterface sh.SessionInterface
+var MockShellInterface sh.SessionInterface
 
 func init() {
-    sh.MainInterface = new(sessionMock)
+	DefaultShInterface = sh.MainInterface
+	MockShellInterface = new(sessionMock)
+	UseMock()
+}
+
+func Reset() {
+	Commands = Commands[:0]
+	ErrorsToReturn = ErrorsToReturn[:0]
+	OutputToRetrun = OutputToRetrun[:0]
+}
+
+func UseDefault() {
+	sh.MainInterface = DefaultShInterface
+}
+
+func UseMock() {
+	sh.MainInterface = MockShellInterface
 }
 
 type sessionMock struct {
-    cmd   string
-    dir   string
-    input string
-    env   map[string]string
+	cmd   string
+	dir   string
+	input string
+	env   map[string]string
 }
 
 func (c *sessionMock) New() sh.SessionInterface {
-    return new(sessionMock)
+	return new(sessionMock)
 }
 
 func (c *sessionMock) Command(name string, arg ...string) sh.SessionInterface {
-    args := append([]string{name}, arg...)
-    c.cmd = strings.Join(args, " ")
-    return c
+	args := append([]string{name}, arg...)
+	c.cmd = strings.Join(args, " ")
+	return c
 }
 
 func (c *sessionMock) SetInput(s string) sh.SessionInterface {
-    c.input = s
-    return c
+	c.input = s
+	return c
 }
 
 func (c *sessionMock) SetDir(s string) sh.SessionInterface {
-    c.dir = s
-    return c
+	c.dir = s
+	return c
 }
 
 func (c *sessionMock) SetPath(path string) sh.SessionInterface {
-    c.env["PATH"] = path
-    return c
+	c.env["PATH"] = path
+	return c
 }
 
 func (c *sessionMock) SetEnv(key, value string) sh.SessionInterface {
-    c.env[key] = value
-    return c
+	c.env[key] = value
+	return c
 }
 
 func (c *sessionMock) Output() ([]byte, error) {
-    return make([]byte, 0), errorReturnValue()
+	Commands = append(Commands, c.cmd)
+
+	return make([]byte, 0), errorReturnValue()
 }
 
 func (c *sessionMock) PrintOutput() error {
-    return errorReturnValue()
+	return errorReturnValue()
 }
 
 func (c *sessionMock) Run() error {
-    Commands = append(Commands, c.cmd)
+	Commands = append(Commands, c.cmd)
 
-    return errorReturnValue()
+	return errorReturnValue()
 }
 
 func errorReturnValue() error {
-    var returnErr error
-    if len(ErrorsToReturn) > 0 {
-        returnErr = ErrorsToReturn[0]
-        ErrorsToReturn = ErrorsToReturn[1:]
-    }
-    return returnErr
+	var returnErr error
+	if len(ErrorsToReturn) > 0 {
+		returnErr = ErrorsToReturn[0]
+		ErrorsToReturn = ErrorsToReturn[1:]
+	}
+	return returnErr
 }
-
